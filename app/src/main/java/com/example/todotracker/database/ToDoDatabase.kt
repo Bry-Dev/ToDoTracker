561a81db68bb8c0c3d6e617d319f5698035777f3
Bry-Dev <43851087+Bry-Dev@users.noreply.github.com>
2021-05-31 23:00:42 +0800

    Setup Database Class with DAO

--- app/src/main/java/com/example/todotracker/database/ToDoDatabase.kt
+++ app/src/main/java/com/example/todotracker/database/ToDoDatabase.kt
@@ -2,6 +2,9 @@ package com.example.todotracker.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.todotracker.converter.DateConverters
+import com.example.todotracker.dao.ToDoDao
 import com.example.todotracker.model.ToDo
 
 @Database(
@@ -9,5 +12,8 @@ import com.example.todotracker.model.ToDo
     version = 1,
     exportSchema = false
 )
+@TypeConverters(DateConverters::class)
 abstract class ToDoDatabase : RoomDatabase() {
+
+    abstract fun toDoDao() : ToDoDao
 }
