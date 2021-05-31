ffa3a1bf999193e4e9cf150e0eb508c03aeedc38
Bry-Dev <43851087+Bry-Dev@users.noreply.github.com>
2021-05-31 22:37:45 +0800

    Setup Ongoing View Model

--- app/src/main/java/com/example/todotracker/ui/ongoing/OngoingViewModel.kt
+++ app/src/main/java/com/example/todotracker/ui/ongoing/OngoingViewModel.kt
@@ -1,13 +1,22 @@
 package com.example.todotracker.ui.ongoing
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import com.example.todotracker.model.ToDo
+import com.example.todotracker.repository.ToDoRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class OngoingViewModel : ViewModel() {
+@HiltViewModel
+class OngoingViewModel
+@Inject constructor(private val toDoRepository: ToDoRepository): ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is notifications Fragment"
     }
     val text: LiveData<String> = _text
+
+    val allToDoInWork : LiveData<List<ToDo>> = toDoRepository.allToDoInWork.asLiveData()
+
+    fun insertToDoInWork(toDo: ToDo) = viewModelScope.launch { toDoRepository.insertToDoData(toDo) }
 }
